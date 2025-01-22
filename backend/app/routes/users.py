9db17b4af7e36b4388e98a0cfa16bd31cd82012f
Tony Mputhia <tonymmputhia@gmail.com>
2025-01-22 23:56:56 +0300

    Modify routes to include user methods

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import crud, schemas, database
 
 router = APIRouter()
 
 @router.get("/")
 def read_root():
     return {"message": "Welcome to Fantasy Fusion API!"}
+
+@router.post("/users/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    return crud.create_user(db, user)
+
+@router.get("/users/", response_model=list[schemas.UserResponse])
+def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(database.get_db)):
+    return crud.get_users(db, skip=skip, limit=limit)
