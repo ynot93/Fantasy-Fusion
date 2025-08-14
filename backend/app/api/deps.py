da8e5f78ad518f24acce7a7566472befc31bb98b
Tony Mputhia <tonymmputhia@gmail.com>
2025-08-14 16:20:35 +0300

    Add get current user to dependencies

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,9 +1,29 @@
-from app.db.database import SessionLocal
+from app.db.database import AsyncSessionLocal
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from jose import JWTError
+from app.db.auth import crud, models
+from app.core.security import decode_token
 
 # Dependency to get a database session
-def get_db():
-    db = SessionLocal()
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
+
+# Dependency to get the current user
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
     try:
-        yield db
-    finally:
-        db.close()
+        payload = decode_token(token)
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        user = await db.get(models.User, user_id)
+        if not user:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired or invalid")
