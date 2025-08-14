aa38afaadf3a0f7ca6305ba8b2070118080bac9b
Tony Mputhia <tonymmputhia@gmail.com>
2025-08-14 16:23:21 +0300

    Change DB session to async session

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-# Create the database engine
-engine = create_engine(settings.DATABASE_URL)
+engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
 
-# Create a sessionmaker
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 
 # Base class for models
 Base = declarative_base()
