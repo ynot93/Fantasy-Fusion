546a35ebf28cd634216f5ed407f8604ca3971d96
Tony Mputhia <tonymmputhia@gmail.com>
2025-08-14 16:20:00 +0300

    Add async context to database creation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-from app.api.routes import auth
+from app.api.routes import auth, leagues
+from contextlib import asynccontextmanager
 from app.db.database import Base, engine
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Fantasy Fusion")
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(leagues.router, prefix="/leagues", tags=["leagues"])
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
