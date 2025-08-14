e08a881dd4aa1d40d0f89056c089bd9ff312b44c
Tony Mputhia <tonymmputhia@gmail.com>
2025-08-14 16:22:49 +0300

    Add decode token and create refresh token functions

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -10,13 +10,19 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
-
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+def create_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
-    to_encode.update({"exp": expire})
+    to_encode["exp"] = datetime.utcnow() + expires_delta
     return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def create_access_token(data: dict):
+    return create_token(data, timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES))
+
+def create_refresh_token(data: dict):
+    return create_token(data, timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS))
+
+def decode_token(token: str):
+    return jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.ALGORITHM])
