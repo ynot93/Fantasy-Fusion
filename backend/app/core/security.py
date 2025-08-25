e44394fabc1d7f3e2c318cf7bda2b9b8aab54adc
Tony Mputhia <tonymmputhia@gmail.com>
2025-08-25 08:16:44 +0300

    Add mpesa acces token function

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -3,9 +3,9 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from typing import Optional
 from app.core.config import settings
+import base64, httpx
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
@@ -26,3 +26,18 @@ def create_refresh_token(data: dict):
 
 def decode_token(token: str):
     return jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.ALGORITHM])
+
+async def get_mpesa_access_token():
+    consumer_key = settings.CONSUMER_KEY
+    consumer_secret = settings.CONSUMER_SECRET
+    
+    auth_string = f"{consumer_key}:{consumer_secret}"
+    encoded_auth_string = base64.b64encode(auth_string.encode()).decode()
+    
+    url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    headers = {"Authorization": f"Basic {encoded_auth_string}"}
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()["access_token"]
